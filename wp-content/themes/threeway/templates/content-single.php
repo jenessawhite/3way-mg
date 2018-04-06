5100e6022ec3a808b59c4a8722a74bfcf0767fea
Jenessa White <jenessaw31@gmail.com>
2018-04-06 11:51:09 -0500

    reworked the layout for the content and single

--- wp-content/themes/threeway/templates/content-single.php
+++ wp-content/themes/threeway/templates/content-single.php
@@ -1,15 +1,24 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
+    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'single-post-thumbnail' );?>
+
+    <?php if ( $image ) : ?>
+      <header class="row text-center <?php if ( $image ) : echo "header-image"; endif; ?>" style="background-image: url(<?php echo $image[0]; ?>)">
+      </header>
+    <?php endif; ?>
+    <div class="entry-content row">
+      <h1 class="entry-title col-12 my-3"><?php the_title(); ?></h1>
+      <div class="col-12">
+        <?php the_content(); ?>
+      </div>
+      <?php if ( get_field('eventbrite_embed') ) : ?>
+      <div class="col-12">
+        <?php echo get_field('eventbrite_embed', false, false); ?>
+      </div>
+    <?php endif; ?>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
--- wp-content/themes/threeway/templates/content.php
+++ wp-content/themes/threeway/templates/content.php
@@ -1,9 +1,30 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
+<?php $classes = array(
+  'entry',
+  'col-12',
+  'col-md-10',
+  'p-0',
+  'text-center',
+  'my-1'
+); ?>
+<div class="row justify-content-center">
+<article <?php post_class($classes); ?>>
+  <header class="graphic-header">
+    <div class="row no-gutters">
+      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'single-post-thumbnail' );?>
+
+      <?php if ( $image ) : ?>
+      <div class="col-12 bg-blue px-4 py-3" style="background: linear-gradient(0deg, rgba(18,78,120,.75) 0%, rgba(18,78,120,.75) 100%), url(<?php echo $image[0]; ?>)">
+      <?php else : ?>
+      <div class="col-12 bg-blue px-4 py-3">
+      <?php endif; ?>
+        <div class="entry-title">
+          <h2><?php the_title(); ?></h2>
+        </div>
+      </div>
+      <div class="col-10 offset-1 mt-2 description">
+        <?php the_excerpt(); ?>
+      </div>
+    </div>
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
 </article>
+</div>
