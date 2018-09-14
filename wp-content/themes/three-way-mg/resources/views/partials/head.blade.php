6597f239f6963c5678e611d82960f9fa92d40fa7
Jenessa White <jenessaw31@gmail.com>
2018-09-14 15:39:21 -0500

    remove old theme

--- wp-content/themes/three-way-mg/resources/views/partials/head.blade.php
+++
@@ -1,8 +0,0 @@
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  @php(wp_head())
-  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Fugaz+One|Montserrat:400,400i,700" rel="stylesheet">
-</head>
